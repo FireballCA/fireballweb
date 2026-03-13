7b96a743b881d07dca5cb98505cc038dde7c42c9
Exo. <143846518+whyyexo@users.noreply.github.com>
2026-03-12 21:53:00 -0400

    s

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -186,7 +186,6 @@ export function Header() {
   return (
     <header
       className="fixed top-0 left-0 right-0 z-50"
-      data-no-smooth-scroll
       style={
         menuOpen
           ? {
