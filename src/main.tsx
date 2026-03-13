47788f9df30b505787cabf9166029d0760afeb10
Exo. <143846518+whyyexo@users.noreply.github.com>
2026-03-12 21:41:15 -0400

    d

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import './i18n'
 import App from './App'
 import './index.css'
 import './styles/business.css'
+import { initSmoothScroll } from './utils/smoothScroll'
+
+// Initialiser le smooth scroll
+initSmoothScroll()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
