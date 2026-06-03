d43b8032676fcc00a8d37b679ee18fbbf2d4a3bc
Exo. <143846518+whyyexo@users.noreply.github.com>
2026-06-03 14:24:06 -0400

    jh

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
 import './i18n'
 import App from './App'
 import { FORCE_FULL_SITE_MOTION } from './constants/motion'
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <LenisRoot>
         <ScrollToTop />
         <App />
+        <Analytics />
       </LenisRoot>
     </BrowserRouter>
   </HelmetProvider>,
